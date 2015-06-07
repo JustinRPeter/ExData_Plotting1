a7c60d43df033a6153bc94524fec97d4c45d8388
JustinRPeter <justinrpeter@gmail.com>
2015-06-07 14:34:35 +1000

    Fixed problems with graph sizes

--- plot1.R
+++ plot1.R
@@ -1,17 +1,13 @@
 #Produces the first plot of Project1 of Exploratory Data Analysis
-
-
 #Justin Peter, 15 June 2015
 
-
 #Read in the data
-source("loadData.R")
-
+    source("loadData.R")
 
 #Produce first plot
-png("plot1.png",width=480,height=480)
-hist(data$Global_active_power,col="red",
-     main="Global Active Power",
-     xlab="Global active power (kilowatts)")
-dev.off()
+    png("plot1.png",width=480,height=480)
+    hist(data$Global_active_power,col="red",
+         main="Global Active Power",
+         xlab="Global active power (kilowatts)")
+    dev.off()
 
--- plot2.R
+++ plot2.R
@@ -1,19 +1,15 @@
 #Produces the second plot of Project1 of Exploratory Data Analysis
 
-
 #Justin Peter, 15 June 2015
-
 
 #Read in the data
 source("loadData.R")
 
-
 #Produce second plot
-png("plot2.png",width=480,height=480)
+    png("plot2.png",width=480,height=480)
 
-plot(data$DateTime,data$Global_active_power,
-     type="l",
-     xlab="",ylab="Global Active Power (kilowatts)")
+    plot(data$DateTime,data$Global_active_power,
+         type="l",
+         xlab="",ylab="Global Active Power (kilowatts)")
 
-dev.off()
-
+    dev.off()
